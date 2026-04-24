057594cfe4613cfa8bd7ce78b3de4d4420aa4ea4
abdulhaseeb0666 <abdulhaseebah7886786@gmail.com>
2026-04-24 10:34:24 +0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import CategoriesPage from './pages/CategoriesPage'
 import Chefs from './pages/Chefs'
 
 const App = () => {
-
-
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/categories" element={<CategoriesPage />} />
-          <Route path="/chefs" element={<Chefs  />} />
+          <Route path="/chefs" element={<Chefs />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
